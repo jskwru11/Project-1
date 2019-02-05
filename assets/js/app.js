bedc969829088d1ee75cf0a20a6e8000b278cca5
Hoo Man <hoomanfor@gmail.com>
2019-02-05 18:38:57 -0500

    fixed modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -179,6 +179,7 @@ $(document).ready(function () {
         //clear the form
         $("#inputFood").val("");
         $("#inputTime").val("");
+        $("#myModal").modal('toggle');
     });
     //on-click event for restaurant selection 
     $(document).on("click", ".restaurant-row", function(){
