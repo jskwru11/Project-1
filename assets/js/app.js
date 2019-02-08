e8245fa1cb4bc636c97c5f69834ec19ed1df50fb
johnale <johnale@cisco.com>
2019-02-08 10:40:26 -0500

    updated version number

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-console.log("v1.401"); //this is updated so you can see when GitHub has actually deployed your code. This is necessary for testing stuff with CORS limitations (like Google Maps)
+console.log("v1.402"); //this is updated so you can see when GitHub has actually deployed your code. This is necessary for testing stuff with CORS limitations (like Google Maps)
 
 var map;
 var userLatitude;
